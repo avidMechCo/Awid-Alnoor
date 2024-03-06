c90e921269f26e6817f21c8cd01ff2cce3cff8b2
negin noori <neginnoorin@gmai.com>
2024-03-06 16:09:33 +0330

    article

--- src/components/blogs/ThreeColSimpleWithImageAndDashedBorder.js
+++ src/components/blogs/ThreeColSimpleWithImageAndDashedBorder.js
@@ -5,9 +5,12 @@ import {css} from "styled-components/macro"; //eslint-disable-line
 import { SectionHeading as HeadingTitle, Subheading } from "components/misc/Headings.js";
 import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
 import { ReactComponent as UserIcon } from "feather-icons/dist/icons/user.svg";
-import { ReactComponent as TagIcon } from "feather-icons/dist/icons/tag.svg";
+//import { ReactComponent as TagIcon } from "feather-icons/dist/icons/tag.svg";
 import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-1.svg";
 import { ReactComponent as SvgDecoratorBlob2 } from "../../images/svg-decorator-blob-3.svg";
+import pic1 from "images/gr11_lrg.jpg"
+import pic2 from "images/pic2.png"
+import pic3 from "images/pic3.png"
 
 const Container = tw.div`relative`;
 const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
@@ -46,38 +49,35 @@ const DecoratorBlob2 = tw(
 )`-z-10 absolute top-0 left-0 w-48 h-48 transform -translate-x-32 translate-y-full opacity-25`;
 
 export default ({
-  subheading = "Blog",
+  subheading = "Articles",
   heading = <>We Love <span tw="text-primary-500">Writing.</span></>,
-  description = "Some amazing blog posts that are written by even more amazing people.",
+  description = "Our team has crafted several remarkable articles that highlight the expertise of our skilled professionals.",
 
 }) => {
   const blogPosts = [
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      author: "Adam Wathan",
-      category: "SEO",
-      title: "Optimizing your website for your main keyword",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      url: "https://reddit.com"
+      pic1 ,
+      author: "Alireza Tavakolian et al.",
+      title: "Fast COVID-19 versus H1N1 screening using optimized parallel inception",
+      description: "COVID-19 and swine-origin influenza A (H1N1) are both pandemics that sparked significant concern worldwide. Since these two diseases have common symptoms, a fast COVID-19 versus H1N1 screening helps better manage patients at healthcare facilities. We present a novel deep model, called Optimized Parallel Inception, for fast screening of COVID-19.",
+      url: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9119711/"
     },
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1479660095429-2cf4e1360472?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      author: "Owais Khan",
-      category: "Advertising",
-      title: "Creating The perfect advertisement campaign",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      url: "https://timerse.com"
+        pic3,
+      author: "Alireza Tavakolian et al.",
+      title: "Source code for optimized parallel inception: A fast COVID-19 screening software",
+      description: "COVID-19 and swine-origin influenza A (H1N1) are both pandemics that sparked significant concern worldwide. These two viruses have the same symptoms and occur at a collision timeline. Optimized Parallel Inception (OPI) presents a new strategy to screen the COVID-19 from H1N1 with use of only symptoms.",
+      url: "https://www.sciencedirect.com/science/article/pii/S2665963822000616"
     },
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1579869847514-7c1a19d2d2ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
-      author: "Steve Schoger",
-      category: "Social Media",
-      title: "Efficient management of your social media assets",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      url: "https://timerse.com"
+      pic2,
+      author: "Alireza Tavakolian et al.",
+      title: "Hospital Readmission and Length of Stay Prediction Using an Optimized Hybrid Deep Model",
+      description: "Hospital readmission and length-of-stay predictions provide information on how to manage hospital bed capacity and the number of required staff, especially during pandemics. We present a hybrid deep model called the Genetic Algorithm-Optimized Convolutional Neural Network (GAOCNN).",
+      url: "https://www.mdpi.com/1999-5903/15/9/304"
     }
   ];
   return (
@@ -100,8 +100,6 @@ export default ({
                       <div>{post.author}</div>
                     </Meta>
                     <Meta>
-                      <TagIcon />
-                      <div>{post.category}</div>
                     </Meta>
                   </MetaContainer>
                   <Title>{post.title}</Title>
