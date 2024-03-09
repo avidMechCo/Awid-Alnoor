21f3253b3ba33d351b92bb977c9230823c05b3ee
negin noori <neginnoorin@gmai.com>
2024-03-09 13:37:21 +0330

    gg

--- src/App.js
+++ src/App.js
@@ -2,26 +2,6 @@ import React from "react";
 import GlobalStyles from 'styles/GlobalStyles';
 import { css } from "styled-components/macro"; //eslint-disable-line
 
-/*
- * This is the entry point component of this project. You can change the below exported default App component to any of
- * the prebuilt landing page components by uncommenting their import and export lines respectively.
- * See one of the landing page components to better understand how to import and render different components (Always
- * make sure if you are building your own page, the root component should be the AnimationRevealPage component. You can
- * disable the animation by using the disabled prop.
- *
- * The App component below is using React router to render the landing page that you see on the live demo website
- * and the component previews.
- *
- */
-
-/* Use AnimationRevealPage as a wrapper component for your pages if you are building a custom one yourself */
-// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-
-/*
- * Hero section is the top most section on the page. It contains the header as well.
- * So you dont need to import headers
- * separately
- */
 
 // import Hero from "components/hero/TwoColumnWithVideo.js";
 // import Hero from "components/hero/TwoColumnWithInput.js";
@@ -50,7 +30,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 // import SliderCard from "components/cards/ThreeColSlider.js";
 // import TrendingCard from "components/cards/TwoTrendingPreviewCardsWithImage.js";
 // import Portfolio from "components/cards/PortfolioTwoCardsWithImage.js";
-// import TabGrid from "components/cards/TabCardGrid.js";
+//import TabGrid from "components/cards/TabCardGrid.js";
 
 // import Blog from "components/blogs/ThreeColSimpleWithImage.js";
 // import Blog from "components/blogs/ThreeColSimpleWithImageAndDashedBorder.js";
@@ -99,6 +79,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 // import BlogIndexPage from "pages/BlogIndex.js";
 // import TermsOfServicePage from "pages/TermsOfService.js";
 // import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
+import AIservices from "pages/AIservices";
 
 import ComponentRenderer from "ComponentRenderer.js";
 import MainLandingPage from "MainLandingPage.js";
@@ -120,6 +101,7 @@ export default function App() {
           <Route path="/components/:type/:name" element={<ComponentRenderer />} />
           <Route path="/thank-you" element={<ThankYouPage />} />
           <Route path="/" element={<MainLandingPage />} />
+          <Route path="/AIservices" element={<AIservices />} />
         </Routes>
       </Router>
     </>
